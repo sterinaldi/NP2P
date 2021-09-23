9b24392291e57dd92c8a745d46ced594e37da96b
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-09-23 18:08:34 +0200

    added random paths sampling

--- ParEst.py
+++ ParEst.py
@@ -11,7 +11,9 @@ import ctypes
 from multiprocessing import Pool
 from itertools import product
 from time import perf_counter
+from compute_draws import random_paths
 import pickle
+import ray
 
 _PTR = ctypes.POINTER
 _dble = ctypes.c_double
@@ -37,31 +39,31 @@ def cartesian_product(arrays):
 def findsubsets(l):
     return np.array([np.log(i) for i in cartesian_product(l) if np.abs(np.sum(i)-1)<1e-4])
 
-def preprocess(N_max, samples, x_min, x_max, n_samps, nthreads = 1):
+def preprocess(N_max, samples, x_min, x_max, nthreads = 1, N_draws = 100, sample_choice = 'shuffle'):
     print('Pre-processing: subsets')
     subsets = {}
-    for N in np.arange(2,N_max):
+    for N in np.arange(N_max-1,N_max):
         m  = np.linspace(x_min, x_max, N)
         dm = m[1] - m[0]
         probs = []
         for samp in samples:
-            p = np.ones(N) * -np.inf
             p = samp(m)
             probs.append(p+np.log(dm))
-        probs.append(np.zeros(N))
         probs = np.array([p - logsumexp(p) for p in probs])
 #        subset = findsubsets(np.exp(probs.T))
-        subset = np.array(probs)
+        subset = random_paths(np.exp(probs).T, N_draws)
         subsets[N] = subset
     return subsets
 
 class DirichletProcess(cpnest.model.Model):
     
-    def __init__(self, model, pars, bounds, samples, x_min, x_max, prior_pars = lambda x: 0, max_a = 10000, max_g = 200, max_N = 300, nthreads = 4, subsets = None, out_folder = './', load_preprocessed = True):
+    def __init__(self, model, pars, bounds, samples, x_min, x_max, prior_pars = lambda x: 0, max_a = 10000, max_g = 200, max_N = 300, nthreads = 4, subsets = None, out_folder = './', load_preprocessed = True, n_draws = -1):
     
         super(DirichletProcess, self).__init__()
         self.samples    = samples
         self.n_samps    = len(samples)
+        if n_draws == -1:
+            n_draws = self.n_samps
         self.labels     = pars
         self.names      = pars + ['a', 'N', 'g']
         self.bounds     = bounds + [[1, max_a], [max_N-1,max_N], [1e-3, max_g]]
@@ -70,32 +72,14 @@ class DirichletProcess(cpnest.model.Model):
         self.x_max      = x_max
         self.model      = model
         self.prior_norm = np.log(np.exp(-self.bounds[-2][0])-np.exp(-self.bounds[-2][1]))
-        if subsets is None:
-            if load_preprocessed:
-                try:
-                    f = open(out_folder + '/subsets.pkl', 'rb')
-                    self.subsets = pickle.load(f)
-                    f.close()
-                except:
-                    self.subsets = preprocess(max_N, samples, x_min, x_max, self.n_samps)
-                    f = open(out_folder + '/subsets.pkl', 'wb')
-                    pickle.dump(self.subsets, f)
-                    f.close()
-            else:
-                self.subsets = preprocess(max_N, samples, x_min, x_max, self.n_samps)
-                f = open(out_folder + '/subsets.pkl', 'wb')
-                pickle.dump(self.subsets, f)
-                f.close()
-        else:
-            self.subsets = subsets
-#        self.p          = Pool(nthreads)
+        self.subsets    = preprocess(max_N, samples, x_min, x_max, N_draws = n_draws)
 
     
     def log_prior(self, x):
     
         logP = super(DirichletProcess,self).log_prior(x)
         if np.isfinite(logP):
-            logP = -(1/x['a']) - (1/x['g']) #- self.prior_norm #-np.log(x['N'])  - x['g']# - self.prior_norm
+            logP = -(1/x['a']) - (1/x['g'])
             pars = [x[lab] for lab in self.labels]
             logP += self.prior_pars(*pars)
         return logP
