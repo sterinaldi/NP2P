a8e63c8eb394c9b7d0d72aa4cc91ee92b3c63641
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-10-26 17:33:05 +0200

    cython log_likelihood

--- ParEst.py
+++ ParEst.py
@@ -1,76 +1,34 @@
 import cpnest.model
-from scipy.special import gammaln, logsumexp, xlogy
-from scipy.stats import gamma, dirichlet, beta
 import numpy as np
-from numba.extending import get_cython_function_address
-from numba import vectorize, njit, jit, prange
-from numpy.random import randint, shuffle
-from random import sample, shuffle
-import matplotlib.pyplot as plt
-import ctypes
-from multiprocessing import Pool
-from itertools import product
-from time import perf_counter
-from compute_draws import random_paths
-import pickle
-import ray
+from loglikelihood import log_likelihood
 
-_PTR = ctypes.POINTER
-_dble = ctypes.c_double
-_ptr_dble = _PTR(_dble)
-
-addr = get_cython_function_address("scipy.special.cython_special", "gammaln")
-functype = ctypes.CFUNCTYPE(_dble, _dble)
-gammaln_float64 = functype(addr)
-
-@jit
-def log_add(x, y): return x+np.log(1.0+np.exp(y-x)) if x >= y else y+np.log(1.0+np.exp(x-y))
-def log_sub(x, y): return x + np.log1p(-np.exp(y-x))
-def log_norm(x, x0, s): return -((x-x0)**2)/(2*s*s) - np.log(np.sqrt(2*np.pi)) - np.log(s)
-
-def cartesian_product(arrays):
-    la = len(arrays)
-    dtype = np.result_type(*arrays)
-    arr = np.empty([la] + [len(a) for a in arrays], dtype='float64')
-    for i, a in enumerate(np.ix_(*arrays)):
-        arr[i, ...] = a
-    yield arr.reshape(la, -1).T
-
-def findsubsets(l):
-    return np.array([np.log(i) for i in cartesian_product(l) if np.abs(np.sum(i)-1)<1e-4])
-
-def preprocess(N_bins, samples, x_min, x_max, nthreads = 1, N_paths = 100, precision = 1e-3):
-    print('Pre-processing: paths')
-    m  = np.linspace(x_min, x_max, N_bins)
-    dm = m[1] - m[0]
+def preprocess(N_bins, samples, x_min, x_max):
+    x  = np.linspace(x_min, x_max, N_bins)
+    dx = x[1] - x[0]
     probs = []
     for samp in samples:
-        p = samp(m)
-        probs.append(p+np.log(dm))
-    probs = [p - logsumexp(p) for p in probs]
-    probs.append(-np.ones(N_bins)*np.inf)
-    probs = np.array(probs)
-    paths = random_paths(np.exp(probs).T, N_paths, precision)
-    return paths
+        p = samp(x)
+        probs.append(p+np.log(dx))
+    probs = np.array([p - logsumexp(p) for p in probs])
+    return probs
 
 class DirichletProcess(cpnest.model.Model):
-    
-    def __init__(self, model, pars, bounds, samples, m_min, m_max, N_paths, prior_pars = lambda x: 0, max_a = 10000, max_g = 5, N_bins = 300, nthreads = 4, subsets = None, out_folder = './', load_preprocessed = True, precision = 1e-3):
+
+    def __init__(self, model, pars, bounds, samples, x_min, x_max, prior_pars = lambda x: 0, max_a = 10000, N_bins = 300, nthreads = 4, out_folder = './'):
     
         super(DirichletProcess, self).__init__()
         self.samples    = samples
         self.N          = len(samples)
         self.labels     = pars
-        self.names      = pars + ['a', 'g']
-        self.bounds     = bounds + [[1, max_a], [1e-3, max_g]]
+        self.names      = pars + ['a']
+        self.bounds     = bounds + [[1, max_a]]
         self.prior_pars = prior_pars
-        self.m_min      = m_min
-        self.m_max      = m_max
+        self.x_min      = x_min
+        self.x_max      = x_max
         self.model      = model
-        self.precision  = precision
-        self.paths      = preprocess(N_bins, samples, m_min, m_max, N_paths = N_paths, precision = self.precision)
-        self.m          = np.linspace(self.m_min, self.m_max, N_bins)
-        self.dm         = self.m[1] - self.m[0]
+        self.draws      = preprocess(N_bins, samples, m_min, m_max)
+        self.x          = np.linspace(self.m_min, self.m_max, N_bins)
+        self.dx         = self.x[1] - self.x[0]
         self.K          = N_bins
 
     
@@ -78,39 +36,10 @@ class DirichletProcess(cpnest.model.Model):
     
         logP = super(DirichletProcess,self).log_prior(x)
         if np.isfinite(logP):
-            logP = -(1/x['a']) - (1/x['g'])
+            logP = -(1/x['a'])
             pars = [x[lab] for lab in self.labels]
 #            logP += self.prior_pars(*pars)
         return logP
     
     def log_likelihood(self, x):
-        K  = self.K
-        N = self.N
-        log_g  = np.log(x['g'])
-        pars = [x[lab] for lab in self.labels]
-        base = self.model(self.m, *pars)*self.dm
-        c_par = x['a']
-        log_a = np.log(c_par)
-        
-        g = x['g']
-        a = c_par*base/base.sum()
-        gammas = np.sum([numba_gammaln(ai) for ai in a])
-        # Scritto esplicito per chiarezza
-        addends = np.ones(len(self.paths))*-np.inf
-        for i, path in enumerate(self.paths):
-            deltas = np.sum(path*(a-1)) #priors are accounted for with zeros in path [p_i^(a_i-1)]
-            prior_ai = a[np.where(path == 0.)]
-            N_prior_bins = len(prior_ai)
-            if N_prior_bins > 0:
-                log_res  = np.log(1 - np.sum(np.exp(path[np.where(path < 0.)]))) # Sum of non-prior probabilities
-                DD_prior = np.sum([log_res*ai + numba_gammaln(ai) for ai in prior_ai]) - numba_gammaln(np.sum(prior_ai))
-                addends[i] = deltas + N_prior_bins*log_g + DD_prior - gammas
-            else:
-                addends[i] = deltas - gammas
-        logL = numba_gammaln(c_par) - K*np.log(g + N) + logsumexp(addends)
-        return logL
-    
-
-@njit
-def numba_gammaln(x):
-  return gammaln_float64(x)
+        return log_likelihood(x, self.x, self.draws, self.model)
