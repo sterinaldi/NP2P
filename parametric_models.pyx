764315985087a258681d9cf9f73b2b0bd1dc2824
Walter Del Pozzo <walter.delpozzo@ligo.org>
2021-10-26 16:43:27 +0200

    vectorised models

--- parametric_models.pyx
+++ parametric_models.pyx
@@ -9,22 +9,34 @@ import numpy as np
 from libc.math cimport exp, sqrt, M_PI
 from scipy.special.cython_special cimport erf
 
-cdef double _normal(double x, double x0, double s):
-    return exp(-((x-x0)**2/(2*s**2)))/(sqrt(2*M_PI)*s)
+cdef np.ndarray[double,mode="c",ndim=1] _normal(np.ndarray[double,mode="c",ndim=1] x, double x0, double s):
+    cdef unsigned int i
+    cdef unsigned int n = x.shape[0]
+    cdef np.ndarray[double,mode="c",ndim=1] res = np.zeros(n,dtype=np.double)
+    cdef double[:] res_view = res
+    for i in range(n):
+        res_view[i] = exp(-((x[i]-x0)**2/(2*s**2)))/(sqrt(2*M_PI)*s)
+    return res
 
-def normal(double x, double x0, double s):
+def normal(np.ndarray[double,mode="c",ndim=1] x, double x0, double s):
     return _normal(x, x0, s)
 
-cdef double _power_law(double x,
-                       double alpha,
-                       double x_min,
-                       double x_max,
-                       double l_min,
-                       double l_max):
-    cdef double f = ((1-alpha)/(x_max**(1-alpha) - x_min**(1-alpha)))*x**(-alpha)*(1+erf((x-x_min)/(l_min)))*(1+erf((x_max-x)/l_max))/4.
-    return f
+cdef np.ndarray[double,mode="c",ndim=1] _power_law(np.ndarray[double,mode="c",ndim=1] x,
+                                                   double alpha,
+                                                   double x_min,
+                                                   double x_max,
+                                                   double l_min,
+                                                   double l_max):
 
-def power_law(double x,
+    cdef unsigned int i
+    cdef unsigned int n = x.shape[0]
+    cdef np.ndarray[double,mode="c",ndim=1] res = np.zeros(n,dtype=np.double)
+    cdef double[:] res_view = res
+    for i in range(n):
+        res_view[i] = ((1-alpha)/(x_max**(1-alpha) - x_min**(1-alpha)))*x**(-alpha)*(1+erf((x-x_min)/(l_min)))*(1+erf((x_max-x)/l_max))/4.
+    return res
+
+def power_law(np.ndarray[double,mode="c",ndim=1] x,
               double alpha,
               double x_min,
               double x_max,
@@ -32,10 +44,10 @@ def power_law(double x,
               double l_max):
     return _power_law(x, alpha, x_min, x_max, l_min, l_max)
 
-def bimodal(double x,double x0,double s0,double x1,double s1, double w):
+def bimodal(np.ndarray[double,mode="c",ndim=1] x,double x0,double s0,double x1,double s1, double w):
     return w*_normal(x,x0,s0)+(1.0-w)*_normal(x,x1,s1)
 
-def power_law_peak(double x,
+def power_law_peak(np.ndarray[double,mode="c",ndim=1] x,
             double alpha,
             double x_min,
             double x_max,
