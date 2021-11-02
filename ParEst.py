3a2eee141ca8d22946b3448cf3f01cbf17dd361f
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-11-02 15:18:52 +0100

    interpolation nodes

--- ParEst.py
+++ ParEst.py
@@ -1,36 +1,24 @@
 import cpnest.model
 import numpy as np
-from loglikelihood import log_likelihood
+from loglikelihood import cython_log_likelihood
 from scipy.special import logsumexp
-
-def preprocess(N_bins, samples, x_min, x_max):
-    x  = np.linspace(x_min, x_max, N_bins)
-    dx = x[1] - x[0]
-    probs = []
-    for samp in samples:
-        p = samp(x)
-        probs.append(p+np.log(dx))
-    probs = np.array([p - logsumexp(p) for p in probs])
-    return probs
 
 class DirichletProcess(cpnest.model.Model):
 
-    def __init__(self, model, pars, bounds, samples, x_min, x_max, prior_pars = lambda x: 0, max_a = 10000, N_bins = 300, nthreads = 4, out_folder = './'):
+    def __init__(self, model, pars, bounds, samples, x, prior_pars = lambda x: 0, max_a = 10000, nthreads = 4, out_folder = './'):
     
         super(DirichletProcess, self).__init__()
         self.samples    = samples
         self.N          = len(samples)
         self.labels     = pars
         self.names      = pars + ['a']
-        self.bounds     = bounds + [[0, max_a]]
+        self.bounds     = bounds + [[0, max_a*len(x)]]
         self.prior_pars = prior_pars
-        self.x_min      = x_min
-        self.x_max      = x_max
         self.model      = model
-        self.draws      = preprocess(N_bins, samples, x_min, x_max)
-        self.x          = np.linspace(self.x_min, self.x_max, N_bins)
+        self.draws      = samples
+        self.x          = x
         self.dx         = np.abs(self.x[1] - self.x[0])
-        self.K          = N_bins
+        self.K          = len(x)
 
     def log_prior(self, x):
     
@@ -42,4 +30,4 @@ class DirichletProcess(cpnest.model.Model):
         return logP
     
     def log_likelihood(self, x):
-        return log_likelihood(x, self.x, self.draws, self.model)
+        return cython_log_likelihood(x, self.x, self.draws, self.model)
