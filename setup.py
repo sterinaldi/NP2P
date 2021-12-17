9eb3230f3372a20f71a0f9019bc19a02bef57411
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-12-17 17:48:51 +0100

    add extensions

--- setup.py
+++ setup.py
@@ -16,17 +16,38 @@ class build_ext(_build_ext):
         __builtins__.__NUMPY_SETUP__ = False
         self.include_dirs.append(numpy.get_include())
 
+with open("requirements.txt") as requires_file:
+    requirements = requires_file.read().split("\n")
+
 ext_modules=[
-             Extension("loglikelihood",
-                       sources=["loglikelihood.pyx"],
+             Extension('parest/loglikelihood',
+                       sources=[os.path.join("parest", "loglikelihood.pyx")],
                        libraries=["m"], # Unix-like specific
                        extra_compile_args=["-O3","-ffast-math"],
-                       include_dirs=[numpy.get_include()]
-                       )
+                       include_dirs=['parest', numpy.get_include()]
+                       ),
+             Extension('parest/models',
+                       sources=[os.path.join("parest", "models.pyx")],
+                       libraries=["m"], # Unix-like specific
+                       extra_compile_args=["-O3","-ffast-math"],
+                       include_dirs=['parest', numpy.get_include()]
+                       ),
              ]
+ext_modules = cythonize(ext_modules)
 
 setup(
-      name = "loglikelihood",
-      ext_modules = cythonize(ext_modules, language_level = "3"),
-      include_dirs=[numpy.get_include()]
-      )
+    name = 'parest',
+    use_scm_version=True,
+    description = 'Parameter estimation from non-parametric inference',
+    author = 'Walter Del Pozzo, Stefano Rinaldi',
+    author_email = 'walter.delpozzo@unipi.it, stefano.rinaldi@phd.unipi.it',
+    url = 'https://github.com/sterinaldi/parametric',
+    python_requires = '>=3.7',
+    packages = ['parest'],
+    include_dirs = [numpy.get_include()],
+    install_requires=requirements,
+    setup_requires=['numpy', 'cython', 'setuptools_scm'],
+    entry_points={},
+    package_data={"": ['*.c', '*.pyx', '*.pxd']},
+    ext_modules=ext_modules,
+    )
